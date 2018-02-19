b4eae48d6214e7d06d6f4f1c37a12cc6a97b7d0b
bhunor456 <balazs.hunoristvan@gmail.com>
2018-02-19 15:17:13 -0100

    munka

--- kocsi.py
+++ kocsi.py
@@ -21,9 +21,9 @@ Motor2A = 5 #29-es láb input 4 sárga kábel
 Motor2B = 6 #31-es láb input 3 zöld kábel
 Motor2E = 13 #33-as láb enable 3,4 lila kábel
 
-GPIO.setup(7,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
+#GPIO.setup(7,GPIO.OUT)
+#GPIO.setup(13,GPIO.OUT)
+#GPIO.setup(15,GPIO.OUT)
 #for x in range (0,7):
     #GPIO.output(7,True)
     #time.sleep(0.125)
